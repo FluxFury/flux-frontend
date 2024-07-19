34f7e9a200d79cc0ad41d1aba36e50d41041cf77
deniskockin <den.kochkin36@mail.ru>
2024-07-19 17:06:00 +0300

    Rewoked app.vue *added navbar.vue component in previous commit *added some styles to app container

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,26 @@
+<script setup>
+import Navbar from "@/components/navbar/navbar.vue"
+</script>
 
 <template>
-  <router-view /> 
+	<el-container>
+		<el-header class="header-container">
+			<navbar/>
+		</el-header>
+		<el-main class="main-container">
+			<router-view />
+		</el-main>
+	</el-container>
 </template>
+
+<style>
+.header-container, .main-container {
+	padding: 0;
+	margin: 0;
+	height: fit-content;
+}
+
+.header-container {
+	margin-bottom: 2rem;
+}
+</style>
